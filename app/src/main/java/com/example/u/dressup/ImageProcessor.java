405560c110331d926bb6ae8a67050b8da27a8221
Rajan Unnikrishnan <ukri82@outlook.com>
2015-04-18 21:28:49 +0200

    better dress image

--- app/src/main/java/com/example/u/dressup/ImageProcessor.java
+++ app/src/main/java/com/example/u/dressup/ImageProcessor.java
@@ -1,5 +1,6 @@
 package com.example.u.dressup;
 
+import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfInt;
@@ -25,23 +26,134 @@ import java.util.Arrays;
 
 public class ImageProcessor
 {
-    public ImageProcessor(Mat anImageBuffer_in) {
+    public ImageProcessor(Mat anImageBuffer_in)
+    {
         myImageMatrix = anImageBuffer_in;
         myImageBuf = new byte[(int) myImageMatrix.total()];
         myImageMatrix.get(0, 0, myImageBuf);
 
-        myVisitedMatrix = new byte[(int) myImageMatrix.total()];
+        //myVisitedMatrix = new byte[(int) myImageMatrix.total()];
 
     }
     public void regionGrow()
     {
-        calculateInitialRange();
-        visitPixel(0, 0);
+        //  Perform initial region growing with an estimate of the background pixel values.
+        {
+            calculateInitialRange();
+            RegionGrower aRegionGrower = new RegionGrower(myImageMatrix, myMinVal, myMaxVal, true);
+            aRegionGrower.grow(0, 0);
 
-        ContourProcessor aContourProc = new ContourProcessor(myImageMatrix.size(), myVisitedMatrix);
-        aContourProc.adjustContours();
+            myVisitedMatrix = Arrays.copyOf(aRegionGrower.getVisitedMatrix(), aRegionGrower.getVisitedMatrix().length);
+        }
+
+        //  Extract the contour boundary of the region and smooth it. Result is a binary image with only the boundary burned to it.
+        Point aSeedPoint = null;
+        {
+            ContourProcessor aContourProc = new ContourProcessor(myImageMatrix.size(), myVisitedMatrix);
+            aContourProc.adjustContours();
+
+            //  Copy the bondary binary image out of the object.
+            byte[] aBuffer = new byte[(int) aContourProc.getContourImage().total()];
+            aContourProc.getContourImage().get(0, 0, aBuffer);
+            myContourImage = Mat.zeros(aContourProc.getContourImage().size(), aContourProc.getContourImage().type());
+            myContourImage.put(0, 0, aBuffer);
+
+            aSeedPoint = aContourProc.getSeedPoint();   //  The seed point to denote the interior of the boundary
+            Log.i("regionGrow", "aSeedPoint :[" + aSeedPoint.x + ", " + aSeedPoint.y + "]");
+        }
+
+        //  Now fill the interior of the above boundary with white pixels so that that dress area is marked clearly. This is the final dress matrix.
+        {
+            RegionGrower aRegionGrower1 = new RegionGrower(myContourImage, (byte) 0, (byte) 0, false);  //  The threshold is 0-0, anything outside this value (=255) is the boundary. Don't go out of it. Also only check for 4-connectivity.
+
+
+            aRegionGrower1.grow((int) aSeedPoint.x, (int) aSeedPoint.y);
+
+            //  The resulting binary image is inverted. i.e, required area is white filled and outside it black. We want the other way round. Invert it.
+            Mat aDressOnlyMatGrayScaleInv = Mat.zeros(myImageMatrix.size(), myImageMatrix.type());
+            aDressOnlyMatGrayScaleInv.setTo(new Scalar(255));
+
+            Mat aVisitedMat = Mat.zeros(myImageMatrix.size(), myImageMatrix.type());
+            aVisitedMat.put(0, 0, aRegionGrower1.getVisitedMatrix());
+
+            Core.subtract(aDressOnlyMatGrayScaleInv, aVisitedMat, aVisitedMat);
+
+            //  Perform one round of dilation of this area to smooth out.
+            Imgproc.dilate(aVisitedMat, aVisitedMat, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5, 5)));
+
+            Core.add(myContourImage, aVisitedMat, aVisitedMat);
+
+            aVisitedMat.get(0, 0, myVisitedMatrix);
+        }
     }
 
+    private class RegionGrower
+    {
+        private Mat myImageMatrix;
+        private byte[] myImageBuf;
+        private byte[] myVisitedMatrix;
+
+        private byte myMinVal = (byte)255;
+        private byte myMaxVal = 0;
+
+        private boolean my8ConnectionNeeded = true;
+
+        public RegionGrower(Mat anImageBuffer_in, byte aMinVal_in, byte aMaxVal_in, boolean a8connNeeded_in)
+        {
+            myImageMatrix = anImageBuffer_in;
+            myImageBuf = new byte[(int) myImageMatrix.total()];
+            myImageMatrix.get(0, 0, myImageBuf);
+
+            myVisitedMatrix = new byte[(int) myImageMatrix.total()];
+
+            myMinVal = aMinVal_in;
+            myMaxVal = aMaxVal_in;
+
+            my8ConnectionNeeded = a8connNeeded_in;
+        }
+
+        public void grow(int x, int y)
+        {
+            visitPixel(x, y);
+        }
+
+        public byte[] getVisitedMatrix()
+        {
+            return myVisitedMatrix;
+
+        }
+
+        private void visitPixel(int anXIndex_in, int aYIndex_in)
+        {
+            int anIndex = anXIndex_in * myImageMatrix.cols() + aYIndex_in;
+            myVisitedMatrix[anIndex] = (byte)255;
+
+            for (int i = anXIndex_in - 1; i <= anXIndex_in + 1; i++)
+            {
+                for (int j = aYIndex_in - 1; j <= aYIndex_in + 1; j++)
+                {
+
+                    if (i >= 0 && j >= 0 && i < myImageMatrix.rows() && j < myImageMatrix.cols())
+                    {
+                        if(!my8ConnectionNeeded)
+                        {
+                            double aDist = Math.pow(i - anXIndex_in, 2) + Math.pow(j - aYIndex_in, 2);
+                            if(aDist > 1)
+                                continue;
+                        }
+
+                        int anCurrentIndex = i * myImageMatrix.cols() + j;
+                        if (myVisitedMatrix[anCurrentIndex] == 0 && myImageBuf[anCurrentIndex] <= myMaxVal && myImageBuf[anCurrentIndex] >= myMinVal)
+                        {
+                            visitPixel(i, j);
+                        }
+                    }
+                }
+            }
+
+
+        }
+    }
 
     private class ContourProcessor
     {
@@ -55,11 +167,44 @@ public class ImageProcessor
         int myNearestContourIndex = -1;
         boolean myNearestContourDirection = false;
 
+
+
+        Mat myHiearchy = null;
+
+        private Mat myContourImage;
+
+
         public ContourProcessor(Size anImageSize_in, byte[] aBinaryImage_in)
         {
             myBinImage = aBinaryImage_in;
             myImageSize = anImageSize_in;
 
+
+        }
+
+        public Point getSeedPoint()
+        {
+            Point aSeedPoint = new Point();
+            aSeedPoint.x = myContourImage.rows() / 2;
+            aSeedPoint.y = myContourImage.cols() / 2;
+
+            byte[] aContBuff = new byte[(int) myContourImage.total()];
+            myContourImage.get(0, 0, aContBuff);
+
+            for(int i = 0; i < myContourImage.rows(); i++)
+            {
+                for(int j = 0; j < myContourImage.cols(); j++)
+                {
+                    int anIndex = (int)(i * myContourImage.cols() + j);
+                    if(aContBuff[anIndex] < 0 && i < myContourImage.rows() - 1 && j < myContourImage.cols() - 1)
+                    {
+                        aSeedPoint.x = i + 1;
+                        aSeedPoint.y = j + 1;
+                        return aSeedPoint;
+                    }
+                }
+            }
+            return aSeedPoint;
         }
 
         private void selectBigContours(List<MatOfPoint> aContours)
@@ -75,7 +220,7 @@ public class ImageProcessor
                 {
                     MatOfPoint contour = aContours.get(idx);
 
-                    printContour(contour);
+                    //printContour(contour);
 
                     Rect rect = Imgproc.boundingRect(new MatOfPoint(contour));
 
@@ -106,7 +251,19 @@ public class ImageProcessor
             for(int jdx = 0; jdx < aContour_in.size().height; jdx++)
             {
                 double[] aPoint = aContour_in.get(jdx, 0);
-                Log.i("aContour_in", "aPoint :[" + aPoint[0] + ", " + aPoint[1] + "]");
+                //Log.i("aContour_in", "aPoint :[" + aPoint[0] + ", " + aPoint[1] + "]");
+
+                int aPrevIndex = jdx - 1;
+                if(aPrevIndex < 0)
+                {
+                    aPrevIndex = (int)aContour_in.size().height - 1;
+                }
+                double[] aPrevPoint = aContour_in.get(aPrevIndex, 0);
+                double aLength = Math.pow(aPoint[0] - aPrevPoint[0], 2) + Math.pow(aPoint[1] - aPrevPoint[1], 2);
+                if(aLength > 2)
+                {
+                    Log.i("Big gap : ", "aPoint :[" + aPoint[0] + ", " + aPoint[1] + "]" + "aPrevPoint :[" + aPrevPoint[0] + ", " + aPrevPoint[1] + "]" + "Index = " + jdx);
+                }
             }
         }
         private int getNextUnvisited()
@@ -121,6 +278,53 @@ public class ImageProcessor
             return -1;
         }
 
+        private List<Point> getLinePoints(int x1, int y1, int x2, int y2)   //  Bresenhaums algo
+        {
+            List<Point> aPointArray = new ArrayList<Point>();
+            int dx = Math.abs(x2 - x1);
+            int dy = Math.abs(y2 - y1);
+
+            int sx = (x1 < x2) ? 1 : -1;
+            int sy = (y1 < y2) ? 1 : -1;
+
+            int err = dx - dy;
+
+            while (true)
+            {
+                aPointArray.add(new Point(x1, y1));
+
+                if (x1 == x2 && y1 == y2) {
+                    break;
+                }
+
+                int e2 = 2 * err;
+
+                if (e2 > -dy) {
+                    err = err - dy;
+                    x1 = x1 + sx;
+                }
+
+                if (e2 < dx) {
+                    err = err + dx;
+                    y1 = y1 + sy;
+                }
+            }
+            return aPointArray;
+        }
+
+        private void fillMissingPoints(double[] aLastPoint, double[] aPoint, MatOfPoint aSingleContour)
+        {
+            List<Point> aNewPoints = getLinePoints((int)aLastPoint[0], (int)aLastPoint[1], (int)aPoint[0], (int)aPoint[1]);
+            for(int i = 1; i < aNewPoints.size() - 1; i++)  //  First and last points are already in the contour
+            {
+                double[] anInterMedPoint = new double[2];
+                anInterMedPoint[0] = aNewPoints.get(i).x;
+                anInterMedPoint[1] = aNewPoints.get(i).y;
+                Mat aRows = Mat.zeros(1, 1, aSingleContour.type());
+                aRows.put(0, 0, anInterMedPoint);
+                aSingleContour.push_back(aRows);
+            }
+        }
         private void addNextContour()
         {
             if(myNearestContourIndex < 0)
@@ -148,6 +352,22 @@ public class ImageProcessor
                     aPoint = aNextContour.get((int) aNextContour.size().height - 1 - jdx, 0);
                 }
 
+                if(jdx == 0 && aSingleContourSize > 0)
+                {
+                    double[] aLastPoint = aSingleContour.get(aSingleContourSize - 1, 0);
+
+                    /*List<Point> aNewPoints = getLinePoints((int)aLastPoint[0], (int)aLastPoint[1], (int)aPoint[0], (int)aPoint[1]);
+                    for(int i = 1; i < aNewPoints.size() - 1; i++)  //  First and last points are already in the contour
+                    {
+                        double[] anInterMedPoint = new double[2];
+                        anInterMedPoint[0] = aNewPoints.get(i).x;
+                        anInterMedPoint[1] = aNewPoints.get(i).y;
+                        Mat aRows = Mat.zeros(1, 1, aNextContour.type());
+                        aRows.put(0, 0, anInterMedPoint);
+                        aSingleContour.push_back(aRows);
+                    }*/
+                    fillMissingPoints(aLastPoint, aPoint, aSingleContour);
+                }
                //Log.i("addNextContour", "aPoint :[" + aPoint[0] + ", " + aPoint[1]);
                 Mat aRows = Mat.zeros(1, 1, aNextContour.type());
                 aRows.put(0, 0, aPoint);
@@ -247,7 +467,6 @@ public class ImageProcessor
         }
         private void joinContours()
         {
-            //mySingleContours.add(new MatOfPoint(myBigContours.get(0)));
             mySingleContours.add(new MatOfPoint());
 
             myVisitedArray = new boolean[myBigContours.size()];
@@ -263,13 +482,15 @@ public class ImageProcessor
 
             }
 
-            Log.i("joinContours", "mySingleContours size :[" + mySingleContours.get(0).size().height);
-            for(int jdx = 0; jdx < mySingleContours.get(0).size().height; jdx++)
+            MatOfPoint aSingleContour = mySingleContours.get(0);
+            int aNumPoints = (int)aSingleContour.size().height;
+            if(aNumPoints > 2)
             {
-                double[] aPoint = null;
-                aPoint = mySingleContours.get(0).get(jdx, 0);
-                Log.i("joinContours", "mySingleContours aPoint :[" + aPoint[0] + ", " + aPoint[1]);
+                double[] aLastPoint = aSingleContour.get(aNumPoints - 1, 0);
+                double[] aFirstPoint = aSingleContour.get(0, 0);
+                fillMissingPoints(aLastPoint, aFirstPoint, aSingleContour);
             }
+
         }
 
         private void smoothContours()
@@ -317,7 +538,12 @@ public class ImageProcessor
             mySingleContours.get(0).release();
             mySingleContours.get(0).push_back(aNewContour);
         }
-        Mat myHiearchy = null;
+
+        public Mat getContourImage()
+        {
+            return myContourImage;
+        }
+
         private void adjustContours()
         {
             Mat aDressOnlyMatGrayScale = Mat.zeros(myImageSize, CvType.CV_8U);
@@ -339,11 +565,13 @@ public class ImageProcessor
 
             joinContours();
 
+            printContour(mySingleContours.get(0));
+
             smoothContours();
 
             myContourImage = Mat.zeros(myImageMatrix.size(), CvType.CV_8U);
-            Imgproc.drawContours(myContourImage, mySingleContours, -1, new Scalar(255, 255, 255));
-            //Imgproc.drawContours(myContourImage, myBigContours, -1, new Scalar(255, 255, 255));
+            Imgproc.drawContours(myContourImage, mySingleContours, -1, new Scalar(255));
+            //Imgproc.drawContours(myContourImage, myBigContours, -1, new Scalar(255, 0, 0));
 
 
         }
@@ -362,29 +590,9 @@ public class ImageProcessor
         return myContourImage;
     }
 
-    public void visitPixel(int anXIndex_in, int aYIndex_in)
-    {
-        int anIndex = anXIndex_in * myImageMatrix.cols() + aYIndex_in;
-        myVisitedMatrix[anIndex] = (byte)255;
 
 
-        for(int i = anXIndex_in - 1; i <= anXIndex_in + 1; i++)
-        {
-            for(int j = aYIndex_in - 1; j <= aYIndex_in + 1; j++)
-            {
-                if(i >= 0 && j >= 0 && i < myImageMatrix.rows() && j < myImageMatrix.cols())
-                {
-                    int anCurrentIndex = i * myImageMatrix.cols() + j;
-                    if(myVisitedMatrix[anCurrentIndex] == 0 && myImageBuf[anCurrentIndex] <= myMaxVal && myImageBuf[anCurrentIndex] >= myMinVal )
-                    {
-                        visitPixel(i, j);
-                    }
-                }
-            }
-        }
-    }
 
-    byte myCurrAvg = -1;
     private void processPixel(int i, int j)
     {
         byte aVal = myImageBuf[i * myImageMatrix.cols() + j];
@@ -440,4 +648,6 @@ public class ImageProcessor
     private byte myMaxVal = 0;
 
     private Mat myContourImage;
+
+    byte myCurrAvg = -1;
 }
