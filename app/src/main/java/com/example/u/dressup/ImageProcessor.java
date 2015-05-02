35593c7515587c9dcecad07b8fa6bc3c5450489d
Rajan Unnikrishnan <ukri82@outlook.com>
2015-05-02 14:37:52 +0200

    improved region growing and s2 support

--- app/src/main/java/com/example/u/dressup/ImageProcessor.java
+++ app/src/main/java/com/example/u/dressup/ImageProcessor.java
@@ -15,9 +15,11 @@ import org.opencv.imgproc.Imgproc;
 import android.util.Log;
 
 import java.lang.Math;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Queue;
 
 /**
  * Created by u on 17.03.2015.
@@ -40,7 +42,7 @@ public class ImageProcessor
         //  Perform initial region growing with an estimate of the background pixel values.
         {
             calculateInitialRange();
-            RegionGrower aRegionGrower = new RegionGrower(myImageMatrix, myMinVal, myMaxVal, true);
+            RegionGrowerQBased aRegionGrower = new RegionGrowerQBased(myImageMatrix, myMinVal, myMaxVal, true);
             aRegionGrower.grow(0, 0);
 
             myVisitedMatrix = Arrays.copyOf(aRegionGrower.getVisitedMatrix(), aRegionGrower.getVisitedMatrix().length);
@@ -193,6 +195,93 @@ public class ImageProcessor
                         if (myVisitedMatrix[anCurrentIndex] == 0 && myImageBuf[anCurrentIndex] <= myMaxVal && myImageBuf[anCurrentIndex] >= myMinVal)
                         {
                             visitPixel(i, j);
+                        }
+                    }
+                }
+            }
+
+
+        }
+    }
+
+    private class RegionGrowerQBased
+    {
+        private Mat myImageMatrix;
+        private byte[] myImageBuf;
+        private byte[] myFilledMatrix;
+
+        private byte[] myVisitedMatrix;
+
+        private Queue<Point> myPoints2BVisited = new LinkedList<>();
+
+
+        private byte myMinVal = (byte)255;
+        private byte myMaxVal = 0;
+
+        private boolean my8ConnectionNeeded = true;
+
+        public RegionGrowerQBased(Mat anImageBuffer_in, byte aMinVal_in, byte aMaxVal_in, boolean a8connNeeded_in)
+        {
+            myImageMatrix = anImageBuffer_in;
+            myImageBuf = new byte[(int) myImageMatrix.total()];
+            myImageMatrix.get(0, 0, myImageBuf);
+
+            myFilledMatrix = new byte[(int) myImageMatrix.total()];
+            myVisitedMatrix = new byte[(int) myImageMatrix.total()];
+
+            myMinVal = aMinVal_in;
+            myMaxVal = aMaxVal_in;
+
+            my8ConnectionNeeded = a8connNeeded_in;
+        }
+
+        public void grow(int x, int y)
+        {
+            myPoints2BVisited.add(new Point(x,y));
+            while(!myPoints2BVisited.isEmpty())
+            {
+                Point p = myPoints2BVisited.remove();
+                visitPixel((int)p.x, (int)p.y);
+            }
+        }
+
+        public byte[] getVisitedMatrix()
+        {
+            return myFilledMatrix;
+
+        }
+
+        private void visitPixel(int anXIndex_in, int aYIndex_in)
+        {
+            int anIndex = anXIndex_in * myImageMatrix.cols() + aYIndex_in;
+            myFilledMatrix[anIndex] = (byte)255;
+
+            for (int i = anXIndex_in - 1; i <= anXIndex_in + 1; i++)
+            {
+                for (int j = aYIndex_in - 1; j <= aYIndex_in + 1; j++)
+                {
+
+                    if (i >= 0 && j >= 0 && i < myImageMatrix.rows() && j < myImageMatrix.cols())
+                    {
+                        int anCurrentIndex = i * myImageMatrix.cols() + j;
+                        if (myVisitedMatrix[anCurrentIndex] == 1)
+                            continue;
+
+                        myVisitedMatrix[anCurrentIndex] = (byte)1;
+                        if(i == anXIndex_in && j == aYIndex_in)
+                            continue;
+
+                        if(!my8ConnectionNeeded)
+                        {
+                            double aDist = Math.pow(i - anXIndex_in, 2) + Math.pow(j - aYIndex_in, 2);
+                            if(aDist > 1)
+                                continue;
+                        }
+
+                        if (myImageBuf[anCurrentIndex] <= myMaxVal && myImageBuf[anCurrentIndex] >= myMinVal)
+                        {
+                            //visitPixel(i, j);
+                            myPoints2BVisited.add(new Point(i,j));
                         }
                     }
                 }
